1c9b508f7add1bc08d9bc8af3aa113298ad6596f
Henge <18427186+Henge9@users.noreply.github.com>
2024-12-12 19:33:21 +0100

    creating player

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=(0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
