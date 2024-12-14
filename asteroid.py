4d2d14ca29c6334dbeffa440972b7fc0fe50b540
Henge <18427186+Henge9@users.noreply.github.com>
2024-12-14 13:15:16 +0100

    splt asteroids when shot

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
+import random
 import pygame
 from circleshape import CircleShape
+from constants import *
 
 
 class Asteroid(CircleShape):
@@ -11,3 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS: return
+        
+        speed = random.randint(40, 100)
+        
+        angle = random.uniform(20, 50)
+        velocity_1 = self.velocity.rotate(angle)
+        velocity_2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid_1  = Asteroid(x=self.position.x, y=self.position.y, radius=new_radius)
+        new_asteroid_1.velocity = velocity_1 * 1.2
+        new_asteroid_2  = Asteroid(x=self.position.x, y=self.position.y, radius=new_radius)
+        new_asteroid_2.velocity = velocity_2 * 1.2
